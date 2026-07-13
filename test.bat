4ccd4f3dac399a4e0a38871f8f51a508b7533855
aj-urmi <afrosejahanu@gmail.com>
2026-07-13 21:09:54 +0600

    Delete test.bat

--- test.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Running test cases from test_input.txt...
-echo ========================================
-calculator.exe < test_input.txt
-echo ========================================
-echo Test completed!
-pause
